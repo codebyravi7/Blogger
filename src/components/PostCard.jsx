21a9c4c677a16192d2f870a883502b3ab8cdc579
Ravikant <ravidreamer7@gmail.com>
2024-07-22 12:47:28 +0530

    new commit

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,24 +1,24 @@
-import React from 'react'
-import appwriteService from "../appwrite/config"
-import {Link} from 'react-router-dom'
+import React from "react";
+import appwriteService from "../appwrite/config";
+import { Link } from "react-router-dom";
 
-function PostCard({$id, title, featuredImage}) {
-    
+function PostCard({ $id, title, featuredImage }) {
   return (
-    <Link to={`/post/${$id}`}>
-        <div className='w-full bg-gray-100 rounded-xl p-4 h-72'>
-            <div className='w-full flex justify-center items-center mb-4 h-48'>
-                <img src={appwriteService.getFilePreview(featuredImage)} alt={title}
-                className='rounded-xl max-w-full max-h-full ' />
-
-            </div>
-            <h2
-            className='text-xl font-bold'
-            >{title}</h2>
+    <div className="main">
+      <Link to={`/post/${$id}`}>
+        <div className="w-full bg-gray-100 rounded-xl p-4 h-72 overflow-hidden">
+          <div className="w-full flex justify-center items-center mb-4 h-48">
+            <img
+              src={appwriteService.getFilePreview(featuredImage)}
+              alt={title}
+              className="rounded-xl max-w-full max-h-full"
+            />
+          </div>
+          <h2 className="text-xl font-bold">{title}</h2>
         </div>
-    </Link>
-  )
+      </Link>
+    </div>
+  );
 }
 
-
-export default PostCard
+export default PostCard;
